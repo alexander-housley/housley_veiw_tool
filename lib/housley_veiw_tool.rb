05a76ba5fb47dd7e608ce3ced3b885b558db71a8
slcwestdevcamp <slcwest@devcamp.com>
2017-06-30 20:19:15 +0000

    Dev tool initial commit

---
+++ lib/housley_veiw_tool.rb
@@ -0,0 +1,6 @@
+require "housley_veiw_tool/version"
+require "housley_veiw_tool/renderer"
+
+module HousleyVeiwTool
+  # Your code goes here...
+end
---
+++ lib/housley_veiw_tool/version.rb
@@ -0,0 +1,3 @@
+module HousleyVeiwTool
+  VERSION = "0.1.0"
+end
